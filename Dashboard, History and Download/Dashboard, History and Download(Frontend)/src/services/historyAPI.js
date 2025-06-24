2b34114ad53de0b955d239bf01ba3040225e6408
shanejacob2312 <shanejacob2312@gmail.com>
2025-06-24 23:34:33 +0530

    Delete Dashboard, History and Download directory
    Redundant

--- Dashboard, History and Download/Dashboard, History and Download(Frontend)/src/services/historyAPI.js
+++
@@ -1,8 +0,0 @@
-export const fetchHistory = async (token) => {
-  const res = await fetch('http://localhost:3000/api/history', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return await res.json();
-};
